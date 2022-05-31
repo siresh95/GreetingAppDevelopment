a9051a44fdd4242e15f88830d913c44c94c9ab13
Resh@308 <reshsingh1995@gmail.com>
2022-06-01 04:50:39 +0530

    Using GreetingController return JSON for different HTTP Methods. Test using curl

---
+++ src/test/java/com/bridgelabz/greetingcontroller/GreetingControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greetingcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
